885e9ea98ed5bb32b03e95a96e2eeceea0268e59
Topstarbubba <31091489+Topstarbubba@users.noreply.github.com>
2019-08-06 23:15:54 +0300

    Add files via upload

---
+++ Task1_2.java
@@ -0,0 +1,14 @@
+package unit1;
+
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(getValue( 44, 21, 98));
+    }
+
+    static double getValue(double a, double b, double c) {
+        return (b + Math.sqrt((Math.pow(b, 2) + 4 * a * c)) / 2 * a - Math.pow(a, 3) *c +Math.pow(b, -2));
+    }
+
+}
