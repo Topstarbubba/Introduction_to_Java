80379388d2341ea25b84d992d295f35858487f58
Topstarbubba <31091489+Topstarbubba@users.noreply.github.com>
2019-08-12 16:00:49 +0300

    task

--- Task2_1.java
+++ Task2_1.java
@@ -2,6 +2,14 @@ package unit1;
 
 public class Task1_5 {
 
+    public static void main(String[] args){
 
+        double a = 21;
+        double b = 64;
+        double c = 59;
+
+        System.out.println(((a - 3) * b / 2) + c);
+
+    }
 
 }
